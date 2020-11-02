06ef7625c2fc2f6cd42d58b3965cab38105a0c55
suxiaoshao <2108412077@qq.com>
2020-11-02 22:35:36 +0800

    feat: Add windowCard component

--- src/components/windowCard/windowCard.tsx
+++ src/components/windowCard/windowCard.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Image, View } from '@tarojs/components';
-import { AtDivider } from 'taro-ui';
+import { Image, Text, View } from '@tarojs/components';
+import { AtButton, AtDivider } from 'taro-ui';
 import './windowCard.scss';
 
 interface WindowCardProps {
@@ -12,11 +12,28 @@ interface WindowCardProps {
   dishList: { name: string; dishId: number }[];
 }
 
-export default function WindowCard(props:WindowCardProps): JSX.Element {
+export default function WindowCard(props: WindowCardProps): JSX.Element {
   return (
     <View className='window-card'>
       <View className='main'>
-        <Image src={props.src} />
+        <View className='left'>
+          <Image src={props.src} />
+          <Text>窗口反馈</Text>
+        </View>
+        <View className='desc'>
+          <View className='title-star'>
+            <Text className='title'>{props.name}</Text>
+            <Text className='star'>{props.star}分</Text>
+          </View>
+          <Text className='desc-content'>{props.desc}</Text>
+          <View className='dish-list-feedback'>
+            {props.dishList.map((value) => (
+              <AtButton className='dish' type='primary' size='small' key={value.dishId}>
+                {value.name}
+              </AtButton>
+            ))}
+          </View>
+        </View>
       </View>
       <AtDivider height={1} className='divider' />
     </View>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Ad } from '@tarojs/components';
+import { View } from '@tarojs/components';
 import * as Taro from '@tarojs/taro';
 import './index.scss';
 import WindowCard from '../../components/windowCard/windowCard';
@@ -25,11 +25,11 @@ export default function Index(): JSX.Element {
     <View className='index'>
       <View className='window-list'>
         <WindowCard
-          desc={''}
-          dishList={[]}
-          name={''}
+          desc={'一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的一家好吃的'}
+          dishList={[{dishId:1,name:'饭饭饭饭饭饭'},{dishId:2,name:'菜菜菜菜菜菜'},{dishId:3,name:'肉肉肉肉肉肉肉'}]}
+          name={'饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店饭店'}
           src={'https://pic2.zhimg.com/50/v2-c2b82cbfbd74c2b012d5d1ee4fcb8c63_hd.jpg'}
-          star={5}
+          star={3.7}
           windowsId={1}
         />
       </View>
