02c452d56e51a122203c9002ce21c0eee914def3
suxiaoshao <2108412077@qq.com>
2020-11-13 10:46:58 +0800

    feat : index page ok

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,39 +1,27 @@
 import * as React from 'react';
-import { View } from '@tarojs/components';
-import { AtLoadMore, AtSegmentedControl } from 'taro-ui';
+import { AtTabs, AtTabsPane } from 'taro-ui';
 import { useState } from 'react';
 import './index.scss';
-import WindowCard from '../../components/windowCard/windowCard';
 import Taber from '../../components/tabar/taber';
-import { useAsyncFunc } from '../../util/hook/useAsyncFunc';
-import { getWindowRecommend, GetWindowDishRecommendData } from '../../util/http/getWindowRecommend';
+import IndexList from './indexList';
+import { View } from '@tarojs/components';
 
 export default function Index(): JSX.Element {
   const [tab, setTab] = useState<number>(0);
-  const [fn, loading, errorString, data] = useAsyncFunc<GetWindowDishRecommendData>(async () => {
-    return await getWindowRecommend(1, 1);
-  });
-  React.useEffect(() => {
-    fn();
-  }, []);
-  React.useEffect(() => {
-    console.log(data);
-  }, [data]);
   return (
     <Taber className='index'>
-      <View className='main'>
-        <AtSegmentedControl
-          current={tab}
-          values={['推荐', '热门', '近期热门']}
-          onClick={(index) => {
-            setTab(index);
-          }}
-        />
-        {loading || errorString != undefined ? (
-          <AtLoadMore moreText={`${errorString},请点击重试`} status={loading ? 'loading' : 'more'} />
-        ) : (
-          data.windowList.map((value) => <WindowCard {...value} key={value.windowId} />)
-        )}
+      <AtTabs
+        current={tab}
+        tabList={[{ title: '推荐' }, { title: '热门' }, { title: '近期热门' }]}
+        onClick={(index) => {
+          setTab(index);
+        }}
+        className='index-tab'
+      />
+      <View className='index-main'>
+        <IndexList type={0} none={tab !== 0} />
+        <IndexList type={1} none={tab !== 1} />
+        <IndexList type={2} none={tab !== 2} />
       </View>
     </Taber>
   );
